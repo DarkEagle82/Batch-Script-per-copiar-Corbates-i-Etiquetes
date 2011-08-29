c6abd0b80f571c63ea53447a3cf66775ab7d8d78
Sergi <jr@nucl3ar.net>
2011-08-29 12:47:52 +0200

    Borrar Backups Script

---
+++ BORRAR_backups.bat
@@ -0,0 +1,17 @@
+@echo off
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\*.bak
+
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CORBATA.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CORBATA1.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CORBATA2.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CORBATA3.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CAIXA_PREPACKING.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CAIXA_SAMO.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CAIXA_MONOCALIBRE.LAB
+del /s \\192.168.1.10\c\ETIQUETAS\Utilitzades\CAIXA_LINEAD.LAB
+
+del /s \\192.168.1.10\c\ETIQUETAS\*.bak
+
+PAUSE
+
+
